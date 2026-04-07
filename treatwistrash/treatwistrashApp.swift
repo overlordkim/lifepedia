7f0d1c3f3284164969e54ff91ed633e3438b9b7e
Qunlin Jin <3501049660@qq.com>
2026-04-07 21:44:13 +0800

    Initial Commit

---
+++ treatwistrash/treatwistrashApp.swift
@@ -0,0 +1,17 @@
+//
+//  treatwistrashApp.swift
+//  treatwistrash
+//
+//  Created by kashorin on 2026/4/7.
+//
+
+import SwiftUI
+
+@main
+struct treatwistrashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
